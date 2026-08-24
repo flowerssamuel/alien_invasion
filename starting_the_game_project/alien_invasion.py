dd4e4a11e41364bca47f975e1280280d6a03aa73
flowerssamuel <samuelflowers29@gmail.com>
2026-08-24 13:09:37 +0100

    Add correct screen size

--- starting_the_game_project/alien_invasion.py
+++ starting_the_game_project/alien_invasion.py
@@ -9,7 +9,7 @@ class AlienInvasion:
         """Initialise the game, and create game resources."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode()
+        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
