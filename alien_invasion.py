8080cfd5e8ce18447f43c4209f3ed4be5edb7227
flowerssamuel <samuelflowers29@gmail.com>
2026-08-25 03:46:43 +0100

    Add logic to move the ship right

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,9 +18,6 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
-        # Set the background colour.
-        self.bg_colour = (230, 230, 230)
-
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -33,6 +30,10 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
