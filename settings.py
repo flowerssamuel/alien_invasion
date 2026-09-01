d283f5ec7ea4bb7fd986615d68826ddb42f19939
flowerssamuel <samuelflowers29@gmail.com>
2026-09-01 06:36:37 +0100

    Make the aliens move right

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ class Settings:
         self.screen_height = 800
         self.bg_colour = (230, 230, 230)
 
-        # Ship settings.
+        # Ship settings
         self.ship_speed = 1.5
 
         # Bullet settings
@@ -17,3 +17,6 @@ class Settings:
         self.bullet_height = 15
         self.bullet_colour = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien settings
+        self.alien_speed = 1.0
