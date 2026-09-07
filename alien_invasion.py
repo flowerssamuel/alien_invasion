2596ea10934f47237885735ededc1c1aa3c61214
flowerssamuel <samuelflowers29@gmail.com>
2026-09-07 04:35:12 +0100

    Reset the score after each game

--- alien_invasion.py
+++ alien_invasion.py
@@ -76,6 +76,7 @@ class AlienInvasion:
 
             # Reset the game statistics.
             self.stats.reset_stats()
+            self.sb.prep_score()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
